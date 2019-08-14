637452c514200195049af5ec7aa924c77bdaa38d
User1 <anuradha.krishnanov7@gmail.com>
2019-08-14 20:58:17 +0530

    initial commit

---
+++ src/main/java/com/hcl/matrimonyapplication/api/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hcl.matrimonyapplication.api.repository;
+/***
+ * @author Anuradha
+ */
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hcl.matrimonyapplication.api.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
